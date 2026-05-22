c034de1bd20a1877945c1856c850d94cd6ed46d6
JIINY <eastrin@gmail.com>
2026-05-22 22:33:34 +0900

    사용이 끝난 DebugLog 삭제

--- src/object/Entity.cpp
+++ src/object/Entity.cpp
@@ -111,11 +111,5 @@ namespace Objects
 
         XMStoreFloat4x4(&worldMatForShader_, local); //셰이더에서 column-major로 전치
         XMStoreFloat4x4(&worldMatForCollider_, local);
-
-        DebugLog("[updateWorldMat] this: %p pos: %.3f, %.3f, %.3f scale: %.3f, %.3f, %.3f", this, position_.x, position_.y, position_.z, scale_.x, scale_.y, scale_.z);
-        DebugLog("[updateWorldMat] this: %p mat _11, _12, _13, _14: %.3f, %.3f, %.3f, %.3f", this, worldMatForShader_._11, worldMatForShader_._12, worldMatForShader_._13, worldMatForShader_._14);
-        DebugLog("[updateWorldMat] this: %p mat _21, _22, _23, _24: %.3f, %.3f, %.3f, %.3f", this, worldMatForShader_._21, worldMatForShader_._22, worldMatForShader_._23, worldMatForShader_._24);
-        DebugLog("[updateWorldMat] this: %p mat _31, _32, _33, _14: %.3f, %.3f, %.3f, %.3f", this, worldMatForShader_._31, worldMatForShader_._32, worldMatForShader_._33, worldMatForShader_._34);
-        DebugLog("[updateWorldMat] this: %p mat _41, _42, _43, _44: %.3f, %.3f, %.3f, %.3f", this, worldMatForShader_._41, worldMatForShader_._42, worldMatForShader_._43, worldMatForShader_._44);
     }
 }
--- src/render/RenderObject.cpp
+++ src/render/RenderObject.cpp
@@ -54,16 +54,6 @@ namespace Render
         if (!isVisible_ || !worldMatBuffer_ || elements_.empty()) { return; }
 
         const Math::Mat4& m = getWorldMatForShader();
-        DebugLog("[Enter] this: %p, override: %s, mat _11, _12, _13, _14: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", m._11, m._12, m._13, m._14);
-        DebugLog("[Enter] this: %p, override: %s, mat _21, _22, _23, _24: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", m._21, m._22, m._23, m._24);
-        DebugLog("[Enter] this: %p, override: %s, mat _31, _32, _33, _34: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", m._31, m._32, m._33, m._34);
-        DebugLog("[Enter] this: %p, override: %s, mat _41, _42, _43, _44: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", m._41, m._42, m._43, m._44);
-
-
         Render::WorldMatConstantBufferData worldMatData;
 
         //DirectXMath행렬을 HLSL로 보낼 때 Transpose(전치)가 필요한 경우가 많음: Row-Major -> Column-Major 변환
@@ -75,16 +65,6 @@ namespace Render
             };
 
         worldMatData.world = getWorldMatForShader();
-
-        //업로드 직전
-        DebugLog("[Upload] this: %p, override: %s, trans  _11, _12, _13, _14: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", worldMatData.world._11, worldMatData.world._12, worldMatData.world._13, worldMatData.world._14);
-        DebugLog("[Upload] this: %p, override: %s, trans  _21, _22, _23, _24: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", worldMatData.world._21, worldMatData.world._22, worldMatData.world._23, worldMatData.world._24);
-        DebugLog("[Upload] this: %p, override: %s, trans  _31, _32, _33, _34: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", worldMatData.world._31, worldMatData.world._32, worldMatData.world._33, worldMatData.world._34);
-        DebugLog("[Upload] this: %p, override: %s, trans  _41, _42, _43, _44: %.3f, %.3f, %.3f, %.3f",
-            this, overridePS ? "WIRE" : "OPAQUE", worldMatData.world._41, worldMatData.world._42, worldMatData.world._43, worldMatData.world._44);
 
         worldMatBuffer_->update(context, worldMatData);
         worldMatBuffer_->bindVS(context, 1);
