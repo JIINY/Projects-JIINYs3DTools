86557e892443bbc02b4db36eb61f4a2e692b8d4e
JIINY <eastrin@gmail.com>
2026-05-23 19:54:02 +0900

    HalfLambert매티리얼 추가

---
+++ shaders/Lit_HalfLambert/HalfLambertMaterial.cpp
@@ -0,0 +1,21 @@
+﻿#include "HalfLambertMaterial.hpp"
+using namespace std;
+using namespace DirectX;
+
+
+namespace Render::Materials
+{
+    void HalfLambertMaterial::initialize(ID3D11Device* device)
+    {
+        Material::initialize(device);
+
+        vsPath_ = L"shaders/Lit_HalfLambert/Lit_VS_HalfLambert.hlsl";
+        psPath_ = L"shaders/Lit_HalfLambert/Lit_PS_HalfLambert.hlsl";
+
+        addProperty<XMFLOAT4>("MaterialColor", XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f));
+        addProperty<float>("Roughness", 0.0f);
+        addProperty<XMFLOAT3>("Padding", XMFLOAT3(0, 0, 0));
+
+        createBuffer(device);
+    }
+}
