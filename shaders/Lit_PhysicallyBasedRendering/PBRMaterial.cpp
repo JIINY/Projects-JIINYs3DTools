e4a327846580adb044ee607f1e5597d955453570
JIINY <eastrin@gmail.com>
2026-05-25 23:45:34 +0900

    PBR매티리얼 추가

---
+++ shaders/Lit_PhysicallyBasedRendering/PBRMaterial.cpp
@@ -0,0 +1,22 @@
+﻿#include "PBRMaterial.hpp"
+using namespace std;
+using namespace DirectX;
+
+
+namespace Render::Materials
+{
+    void PBRMaterial::initialize(ID3D11Device* device)
+    {
+        Material::initialize(device);
+
+        vsPath_ = L"shaders/Lit_BlinnPhong/Lit_VS_BlinnPhong.hlsl";
+        psPath_ = L"shaders/Lit_PhysicallyBasedRendering/Lit_PS_PBR.hlsl";
+
+        addProperty<XMFLOAT4>("MaterialColor", XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f));
+        addProperty<float>("Roughness", 0.0f);
+        addProperty<float>("Metallic", 0.0f);
+        addProperty<XMFLOAT2>("Padding", XMFLOAT2(0, 0));
+
+        createBuffer(device);
+    }
+}
---
+++ shaders/Lit_PhysicallyBasedRendering/PBRMaterial.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "common/Math.hpp"
+#include "../src/render/Material.hpp"
+
+
+namespace Render::Materials
+{
+    class PBRMaterial : public Material
+    {
+    public:
+        virtual ~PBRMaterial() = default;
+
+        void initialize(ID3D11Device* device) override;
+
+        void setBaseColor(float r, float g, float b, float a = 1.0f) { setColor("MaterialColor", Math::Vec4(r, g, b, a)); }
+        void setRoughness(float r) { setProperty("Roughness", r); }
+        void setMetallic(float m) { setProperty("Metallic", m); }
+    };
+}
