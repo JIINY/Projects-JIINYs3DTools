ef1a6692bdffc5a6db1c46e2485fb57aa4fafc36
JIINY <eastrin@gmail.com>
2026-05-26 16:39:21 +0900

    FontUtil이 헤더만 있어서 include한 쪽에서 중복 정의되는 문제 해결을 위해 cpp 분리

---
+++ src/utils/FontUtils.cpp
@@ -0,0 +1,12 @@
+﻿#include "FontUtils.hpp"
+#include <string>
+#include <filesystem>
+using namespace std;
+
+string toUTF8(const string& str)
+{
+    filesystem::path path(str);
+    auto u8str = path.u8string();
+
+    return string(reinterpret_cast<const char*>(u8str.c_str()));
+}
--- src/utils/FontUtils.hpp
+++ src/utils/FontUtils.hpp
@@ -1,14 +1,7 @@
 ﻿#pragma once
 #include <string>
-#include <filesystem>
 
 
-#define utf8(text) reinterpret_cast<const char*>(u8##text)
+#define utf8(text) reinterpret_cast<const char*>(u8##text) //컴파일타임 리터럴(u8"...")을 const char*로 캐스팅
 
-std::string toUTF8(const std::string& str) 
-{
-    std::filesystem::path path(str);
-    auto u8str = path.u8string();
-
-    return std::string(reinterpret_cast<const char*>(u8str.c_str()));
-}
+std::string toUTF8(const std::string& str); //런타임 std::string을 받아서 filesystem::path를 거쳐 변환
