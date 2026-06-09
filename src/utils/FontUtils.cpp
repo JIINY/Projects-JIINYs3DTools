8e01f592008f9668c42ac34052ce528c5e6cd0d3
JIINY <eastrin@gmail.com>
2026-06-10 01:46:56 +0900

    Wstring 과 string 변환 추가

--- src/utils/FontUtils.cpp
+++ src/utils/FontUtils.cpp
@@ -1,7 +1,9 @@
 ﻿#include "FontUtils.hpp"
 #include <string>
 #include <filesystem>
+#include <Windows.h>
 using namespace std;
+
 
 string toUTF8(const string& str)
 {
@@ -9,4 +11,25 @@ string toUTF8(const string& str)
     auto u8str = path.u8string();
 
     return string(reinterpret_cast<const char*>(u8str.c_str()));
+}
+
+string convertWstrToStr(const wstring& w)
+{
+    if (w.empty()) { return {}; }
+
+    int size = WideCharToMultiByte(CP_UTF8, 0, w.data(), (int)w.size(), nullptr, 0, nullptr, nullptr);
+    string s(size, 0);
+    WideCharToMultiByte(CP_UTF8, 0, w.data(), (int)w.size(), s.data(), size, nullptr, nullptr);
+
+    return s;
+}
+
+wstring convertStrToWstr(const string& s)
+{
+    if (s.empty()) { return {}; }
+
+    int size = MultiByteToWideChar(CP_UTF8, 0, s.data(), (int)s.size(), nullptr, 0);
+    wstring w(size, 0);
+    MultiByteToWideChar(CP_UTF8, 0, s.data(), (int)s.size(), w.data(), size);
+    return w;
 }
--- src/utils/FontUtils.hpp
+++ src/utils/FontUtils.hpp
@@ -5,3 +5,5 @@
 #define utf8(text) reinterpret_cast<const char*>(u8##text) //컴파일타임 리터럴(u8"...")을 const char*로 캐스팅
 
 std::string toUTF8(const std::string& str); //런타임 std::string을 받아서 filesystem::path를 거쳐 변환
+std::string convertWstrToStr(const std::wstring& w);
+std::wstring convertStrToWstr(const std::string& s);
