d29c77b1fb292cb259a17926ac496676379c4c29
JIINY <eastrin@gmail.com>
2026-06-10 02:17:58 +0900

    기본 SceneFile 준비

--- src/core/PathConfig.hpp
+++ src/core/PathConfig.hpp
@@ -7,3 +7,9 @@ namespace EnvPathConfig
     const std::string defaultFilePath_ = "src/defaults/DefaultEnvironmentConfig.json";
     const std::string userFilePath_ = "assets/environments/";
 }
+
+namespace ScenePathConfig
+{
+    const std::string defaultFilePath_ = "src/defaults/DefaultScene.json";
+    const std::string userFilePath_ = "assets/scenes/";
+}
