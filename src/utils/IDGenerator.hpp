2811510cc385e5ea8d3227cc25b0aeea0cb3fb48
JIINY <eastrin@gmail.com>
2026-06-10 01:44:50 +0900

    객체들의 ID생성

---
+++ src/utils/IDGenerator.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <random>
+#include <cstdint>
+#include <mutex>
+
+
+class IDGenerator
+{
+public:
+    IDGenerator() = delete;
+
+    inline static uint64_t generate()
+    {
+        static std::mt19937_64 engine{ std::random_device{}() };
+        static std::uniform_int_distribution<uint64_t> dist;
+
+        static std::mutex mtx;
+        std::lock_guard<std::mutex> lock(mtx);
+
+        return dist(engine);
+    }
+};
