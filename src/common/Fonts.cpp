b5d22b152fb235f43d62ff01137d2dc12a0ade59
JIINY <eastrin@gmail.com>
2026-06-13 23:27:52 +0900

    오타 수정

--- src/common/Fonts.cpp
+++ src/common/Fonts.cpp
@@ -25,7 +25,7 @@ namespace Fonts
 		Regular17 = LoadFontFromMemory("resources/fonts/NanumHumanRegular.ttf", 17.0f, "Regular17");
 		Regular18 = LoadFontFromMemory("resources/fonts/NanumHumanRegular.ttf", 18.0f, "Regular18");
 		Light18 = LoadFontFromMemory("resources/fonts/NanumHumanLight.ttf", 18.0f, "Light18");
-		Heavy17 = LoadFontFromMemory("resources/fonts/NunumHumanHeavy.ttf", 17.0f, "Heavy17");
+		Heavy17 = LoadFontFromMemory("resources/fonts/NanumHumanHeavy.ttf", 17.0f, "Heavy17");
 		Heavy20 = LoadFontFromMemory("resources/fonts/NanumHumanHeavy.ttf", 20.0f, "Heavy20");
 
 		io.Fonts->FontLoaderFlags = ImGuiFreeTypeBuilderFlags_LightHinting; //작은 글자를 그릴 때 래스터라이저에 힌팅 가이드라인 제공
